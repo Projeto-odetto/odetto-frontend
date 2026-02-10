ecc42e250780395fdb9e6e9c8452a70caf6f174c
Lucas Oliveira <lucas.gracas@germinare.org.br>
2026-02-10 14:04:01 -0300

    feat: implementando input multiline

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/header/header'
-import TextInput from './components/textInput/textInput'
 import Button from './components/button/button';
+import InputMultiline from './components/inputMultiline/inputMultiline';
 
 function App() {
   const [texto, setTexto] = useState("Texto");
@@ -11,7 +11,7 @@ function App() {
     <>
       <Header/>
 
-      <TextInput value={texto} onChange={setTexto} size="md"/>
+      <InputMultiline value={texto} placeholder="Insira um texto..." onChange={setTexto}/>
 
       <Button content="Botão" size='md' onClick={() => {console.log(texto)}}/>
     </>
