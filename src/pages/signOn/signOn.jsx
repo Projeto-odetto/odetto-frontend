edab07248d97ed0243fd78948e036b1ced437310
Lucas Oliveira <lucas.gracas@germinare.org.br>
2026-02-12 09:36:55 -0300

    feat: implementando tela de cadastro

--- src/pages/signOn/signOn.jsx
+++ src/pages/signOn/signOn.jsx
@@ -1,24 +1,41 @@
-import styles from './signIn.module.css'
+import styles from './signOn.module.css'
 import TextInput from '../../components/textInput/textInput'
 import Button from '../../components/button/button'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-function SignIn() {
+function SignOn() {
     const navigate = useNavigate()
 
+    const [name, setName] = useState("")
+    const [subscription, setSubscription] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
 
     return (
         <section className={styles.bg}>
             <div className={styles.content}>
                 <div className={styles.title}>
                     <img src="/odetto-logo-black.svg" alt="logo"/>
-                    <h1>Login</h1>
+                    <h1>Cadastro</h1>
                 </div>
 
                 <form className={styles.form}>
+                    <TextInput
+                        placeholder='Nome Completo'
+                        value={name}
+                        onChange={setName}
+                        size='xlg'
+                    />
+
+                    <TextInput
+                        placeholder='Matrícula'
+                        value={subscription}
+                        onChange={setSubscription}
+                        size='xlg'
+                    />
+
                     <TextInput
                         placeholder='E-mail'
                         value={email}
@@ -32,16 +49,23 @@ function SignIn() {
                         onChange={setPassword}
                         size='xlg'
                     />
+                    
+                    <TextInput
+                        placeholder='Confirmar Senha'
+                        value={confirmPassword}
+                        onChange={setConfirmPassword}
+                        size='xlg'
+                    />
 
                     <div className={styles.actions}>
                         <div className={styles.buttons}>
                             <Button
-                                content='Cadastro'
-                                onClick={() => {navigate("/sign-on")}}
+                                content='Login'
+                                onClick={() => {navigate('/sign-in')}}
                             />
 
                             <Button
-                                content='Fazer Login'
+                                content='Fazer Cadastro'
                                 type='submit'
                             />
                         </div>
@@ -54,4 +78,4 @@ function SignIn() {
     )
 }
 
-export default SignIn
+export default SignOn
--- src/routes/appRoutes.jsx
+++ src/routes/appRoutes.jsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import SignIn from "../pages/signIn/signIn"
+import SignOn from "../pages/signOn/signOn"
 
 export function AppRoutes() {
     return (
         <Routes>
             <Route path='/' element={<Navigate to='/sign-in'/>}/>
             <Route path='/sign-in' element={<SignIn/>}/>
+            <Route path='/sign-on' element={<SignOn/>}/>
         </Routes>
     )
 }
