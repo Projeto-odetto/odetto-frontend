9b8204d8e4542051a7555486bb24eeb0a74530cd
Lucas Oliveira <lucas.gracas@germinare.org.br>
2026-03-01 11:45:41 -0300

    feat: criando tela de aluno

--- src/routes/appRoutes.jsx
+++ src/routes/appRoutes.jsx
@@ -3,6 +3,8 @@ import SignIn from "../pages/signIn/signIn"
 import SignOn from "../pages/signOn/signOn"
 import Teacher from "../pages/teacher/teacher"
 import { studentsMock } from "../mocks/students.mock"
+import { observationsMock } from "../mocks/observations.mock"
+import Student from "../pages/student/student"
 
 export function AppRoutes() {
     return (
@@ -11,6 +13,92 @@ export function AppRoutes() {
             <Route path='/sign-in' element={<SignIn/>}/>
             <Route path='/sign-on' element={<SignOn/>}/>
             <Route path='/teacher' element={<Teacher students={studentsMock}/>}/>
+            <Route path='/student' element={<Student observations={observationsMock} gradesData={[
+                {
+                    subject: "Mobile",
+                    teacher: "Nisflei",
+                    grades: [10, 7, 8.5],
+                    mean: 8.5
+                },
+                {
+                    subject: "Mobile",
+                    teacher: "Nisflei",
+                    grades: [10, 7, 8.5],
+                    mean: 8.5
+                },
+                {
+                    subject: "Mobile",
+                    teacher: "Nisflei",
+                    grades: [10, 7, 8.5],
+                    mean: 8.5
+                },
+                {
+                    subject: "Mobile",
+                    teacher: "Nisflei",
+                    grades: [10, 7, 8.5],
+                    mean: 8.5
+                },
+                {
+                    subject: "Mobile",
+                    teacher: "Nisflei",
+                    grades: [10, 7, 8.5],
+                    mean: 8.5
+                },
+                {
+                    subject: "Mobile",
+                    teacher: "Nisflei",
+                    grades: [10, 7, 8.5],
+                    mean: 8.5
+                },
+                {
+                    subject: "Mobile",
+                    teacher: "Nisflei",
+                    grades: [10, 7, 8.5],
+                    mean: 8.5
+                },
+                {
+                    subject: "Mobile",
+                    teacher: "Nisflei",
+                    grades: [10, 7, 8.5],
+                    mean: 8.5
+                },
+                {
+                    subject: "Mobile",
+                    teacher: "Nisflei",
+                    grades: [10, 7, 8.5],
+                    mean: 8.5
+                },
+                {
+                    subject: "Mobile",
+                    teacher: "Nisflei",
+                    grades: [10, 7, 8.5],
+                    mean: 8.5
+                },
+                {
+                    subject: "Mobile",
+                    teacher: "Nisflei",
+                    grades: [10, 7, 8.5],
+                    mean: 8.5
+                },
+                {
+                    subject: "Mobile",
+                    teacher: "Nisflei",
+                    grades: [10, 7, 8.5],
+                    mean: 8.5
+                },
+                {
+                    subject: "Mobile",
+                    teacher: "Nisflei",
+                    grades: [10, 7, 8.5],
+                    mean: 8.5
+                },
+                {
+                    subject: "Mobile",
+                    teacher: "Nisflei",
+                    grades: [10, 7, 8.5],
+                    mean: 8.5
+                },
+            ]}/>}/>
         </Routes>
     )
 }
